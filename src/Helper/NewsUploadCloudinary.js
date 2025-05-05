17a038e3b9a53ed7e9ff658a2bbd07b79b8b9ac2
huynhtrung1904 <huynhtrung1904@gmail.com>
2025-05-05 16:51:19 +0700

    cloundinary

---
+++ src/Helper/NewsUploadCloudinary.js
@@ -0,0 +1,18 @@
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+require("dotenv").config();
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "news", // Tên folder trên Cloudinary
+    allowed_formats: ["jpg", "jpeg", "png", "gif"], // Định dạng cho phép
+  },
+});
+const uploadCloudinary = multer({ storage });
+module.exports = uploadCloudinary
